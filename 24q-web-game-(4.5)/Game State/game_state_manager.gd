6ac8016b1382bd89460ae26bc9c5211a1f4b229f
JRock <50375578+JoshRocks@users.noreply.github.com>
2026-01-30 13:49:09 -0500

    Meltdown

--- 24q-web-game-(4.5)/Game State/game_state_manager.gd
+++ 24q-web-game-(4.5)/Game State/game_state_manager.gd
@@ -5,7 +5,7 @@ var in_meltdown:bool = true
 func start_meltdown():
 	in_meltdown = true
 	var meltdown_timer = get_tree().get_first_node_in_group("Meltdown_Timer") as Timer
-	var meltdown_bar = get_tree().get_first_node_in_group("Meltdown_Bar") as TextureProgressBar #TODO: REPLACE WITH TEXTURE PROGRESS BAR
+	var meltdown_bar = get_tree().get_first_node_in_group("Meltdown_Bar") as ProgressBar #TODO: REPLACE WITH TEXTURE PROGRESS BAR
 	meltdown_bar.value = 0
 	
 	if meltdown_timer.paused:
@@ -16,15 +16,12 @@ func start_meltdown():
 
 
 
-func pause_meltdown():
+
+
+func stop_meltdown():
 	in_meltdown = false
 	var meltdown_timer = get_tree().get_first_node_in_group("meltdown_timer") as Timer
 	meltdown_timer.paused = true
-
-func resume_meltdown():
-	in_meltdown = true
-	var meltdown_timer = get_tree().get_first_node_in_group("meltdown_timer") as Timer
-	meltdown_timer.paused = false
 
 func game_over() -> void:
 	print("Game Over")
--- 24q-web-game-(4.5)/game_level.gd
+++ 24q-web-game-(4.5)/game_level.gd
@@ -2,25 +2,23 @@ extends Node2D
 
 
 @onready var meltdown_timer: Timer = %"Meltdown Timer"
-@onready var meltdown_bar: TextureProgressBar = $"Meltdown_UI/HBoxContainer/Meltdown Bar"
+@onready var meltdown_bar: ProgressBar = $"Meltdown_UI/HBoxContainer/Meltdown Bar"
 
 
 func _ready() -> void:
 	Game_State_Manager.start_meltdown()
 
 func _process(delta):
-	
 	if Game_State_Manager.in_meltdown:
-		update_meltdown_progress_bar()
+		update_meltdown_timer()
 
 
 
-func update_meltdown_progress_bar():
+func update_meltdown_timer():
 	# Calculate progress: (time left / total time)
 	# Subtract from 1.0 to make it fill as time *decreases*
 	var progress_ratio = (meltdown_timer.wait_time - meltdown_timer.time_left) / meltdown_timer.wait_time
-	meltdown_bar.value = 100 - (progress_ratio * 100) # Set as percentage (0-100)
+	meltdown_bar.value = progress_ratio * 100 # Set as percentage (0-100)
 
 func _on_meltdown_timer_timeout() -> void:
 	Game_State_Manager.game_over()
-	
