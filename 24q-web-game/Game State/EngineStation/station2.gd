40c2125e983f4db49d189855c46175cd727c8081
juicy <wopjr1234@gmail.com>
2026-01-30 15:31:14 -0500

    Adding interactables

---
+++ 24q-web-game/Game State/EngineStation/station2.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+@onready var interactable: Area2D = $Interactable
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	interactable.interact = _on_interact
+	
+func _on_interact():
+	if animated_sprite_2d.frame == 0:
+		animated_sprite_2d.frame = 1
+		interactable.is_interactable = false
+		print("The play is working on a station")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
