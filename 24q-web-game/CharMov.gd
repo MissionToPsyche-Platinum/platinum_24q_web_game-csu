adbfee7c180e7a1b05a4e1e326349f2998d604eb
Nazeer Ahmad <nazeer.finance20@gmail.com>
2026-02-22 22:26:12 -0500

    fixed character model and sounds

--- 24q-web-game/CharMov.gd
+++ 24q-web-game/CharMov.gd
@@ -2,32 +2,32 @@ extends CharacterBody2D
 
 @export var speed : float = 300.0
 @onready var animated_sprite = $Player
+@onready var walk_metal = $Walk_Sounds
 
 func _physics_process(delta):
 	var direction_x = Input.get_axis("ui_left", "ui_right")
 	var direction_y = Input.get_axis("ui_up", "ui_down")
 	
-	# Apply velocity
 	velocity.x = direction_x * speed
 	velocity.y = direction_y * speed
-	
-	# Play animations based on direction
-	# Priority: check which direction has input
+
+	# Animation
 	if direction_y < 0:
-		# Moving up
 		animated_sprite.play("up-forward")
 	elif direction_y > 0:
-		# Moving down
 		animated_sprite.play("down-foward")
 	elif direction_x > 0:
-		# Moving right
 		animated_sprite.play("right_walk")
 	elif direction_x < 0:
-		# Moving left
 		animated_sprite.play("left_walk")
 	else:
-		# Not moving
 		animated_sprite.stop()
-		# Or use idle: animated_sprite.play("idle")
-	
+
+	# Sound - play full audio, only restart if stopped
+	if direction_x != 0 or direction_y != 0:
+		if not walk_metal.playing:
+			walk_metal.play()
+	else:
+		walk_metal.stop()
+
 	move_and_slide()
