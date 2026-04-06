ed50a4f33902b7113507e711bd2a2c2467f71ea9
JRock <50375578+JoshRocks@users.noreply.github.com>
2026-04-06 14:40:20 -0400

    JR Minigame Backend Updates

--- 24q-web-game/Game State/Stations/Engine/station2.gd
+++ 24q-web-game/Game State/Stations/Engine/station2.gd
@@ -2,14 +2,20 @@ extends StaticBody2D
 
 @onready var interactable: Area2D = $Interactable
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var minigame_controller = $MinigameController
+
 # This is what is called when a player interacts with an object
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	interactable.interact = _on_interact
+	minigame_controller.connect("minigame_completed", Callable(self, "_on_minigame_done"))
+	
 	
 func _on_interact():
-	interactable.is_interactable = false
 	print("The play is working on a station")
+	interactable.is_interactable = false
+	minigame_controller.start_minigame()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
