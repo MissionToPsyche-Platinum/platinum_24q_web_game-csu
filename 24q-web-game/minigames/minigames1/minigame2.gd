4e1a0e15fc67f6cf046c2be435348d757964db24
juicy <wopjr1234@gmail.com>
2026-04-10 11:46:04 -0400

    Finished minigame1 and its variations

--- 24q-web-game/minigames/minigames1/minigame2.gd
+++ 24q-web-game/minigames/minigames1/minigame2.gd
@@ -1,17 +1,19 @@
-extends Node
+extends Node2D
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var button1: Area2D = $AnimatedSprite2D/Button1
 @onready var button2: Area2D = $AnimatedSprite2D/Button2
 @onready var button3: Area2D = $AnimatedSprite2D/Button3
 @onready var button4: Area2D = $AnimatedSprite2D/Button4
-@onready var correctArray = [2, 4, 1, 3]
+@onready var correctArray = [2, 1, 3, 4]
 @onready var answerArray = []
 @onready var i = 0
 @onready var click = $click
 @onready var error = $error
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -29,20 +31,19 @@ func _on_button_1_input_event(viewport: Node, event: InputEvent, shape_idx: int)
 			answerArray.clear()
 		else:
 			click.play()
-			sprite.set_frame_and_progress(3, 3)
+			sprite.set_frame_and_progress(2, 2)
 			i = i + 1 
 		print(answerArray)
 		if answerArray == correctArray:
 			#This is when the minigame is done
 			print('done')
-		
 
 
 func _on_button_2_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
 		answerArray.append(2)
 		if answerArray[i] != correctArray[i]:
-			#If wrong button is pressed
+			#If the wrong button is pressed
 			error.play()
 			sprite.set_frame_and_progress(0, 0)
 			i = 0
@@ -50,7 +51,7 @@ func _on_button_2_input_event(viewport: Node, event: InputEvent, shape_idx: int)
 		else:
 			click.play()
 			sprite.set_frame_and_progress(1, 1)
-			i = i + 1
+			i = i + 1 
 		print(answerArray)
 		if answerArray == correctArray:
 			#This is when the minigame is done
@@ -68,8 +69,8 @@ func _on_button_3_input_event(viewport: Node, event: InputEvent, shape_idx: int)
 			answerArray.clear()
 		else:
 			click.play()
-			sprite.set_frame_and_progress(4, 4)
-			i = i + 1
+			sprite.set_frame_and_progress(3, 3)
+			i = i + 1 
 		print(answerArray)
 		if answerArray == correctArray:
 			#This is when the minigame is done
@@ -80,15 +81,15 @@ func _on_button_4_input_event(viewport: Node, event: InputEvent, shape_idx: int)
 	if event is InputEventMouseButton and event.is_pressed():
 		answerArray.append(4)
 		if answerArray[i] != correctArray[i]:
-			#If the wrong  button is pressed
+			#If the wrong button is pressed
 			error.play()
 			sprite.set_frame_and_progress(0, 0)
 			i = 0
 			answerArray.clear()
 		else:
 			click.play()
-			sprite.set_frame_and_progress(2, 2)
-			i = i + 1
+			sprite.set_frame_and_progress(4, 4)
+			i = i + 1 
 		print(answerArray)
 		if answerArray == correctArray:
 			#This is when the minigame is done
